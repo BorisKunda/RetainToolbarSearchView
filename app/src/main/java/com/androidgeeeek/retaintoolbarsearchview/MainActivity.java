5d64e9cfee8b4ae0e4d36748b784351f3e0e7ac9
feorin94 <feorin94@gmail.com>
2017-10-15 16:32:38 +0100

    Add searchview icon to toolbar

--- app/src/main/java/com/androidgeeeek/retaintoolbarsearchview/MainActivity.java
+++ app/src/main/java/com/androidgeeeek/retaintoolbarsearchview/MainActivity.java
@@ -3,6 +3,7 @@ package com.androidgeeeek.retaintoolbarsearchview;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.activity_main_menu, menu);
+        return true;
     }
 }
