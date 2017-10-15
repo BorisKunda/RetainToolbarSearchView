bcecdc2b187f4c0484c56269b0373eec093c9d04
feorin94 <feorin94@gmail.com>
2017-10-15 16:27:03 +0100

    Add toolbar

--- app/src/main/java/com/androidgeeeek/retaintoolbarsearchview/MainActivity.java
+++ app/src/main/java/com/androidgeeeek/retaintoolbarsearchview/MainActivity.java
@@ -1,13 +1,21 @@
 package com.androidgeeeek.retaintoolbarsearchview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    // set up toolbar
+    // https://developer.android.com/training/appbar/setting-up.html
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
     }
 }
